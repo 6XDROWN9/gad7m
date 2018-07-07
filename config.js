603f43e94e92e29c4f173ddba4fc04df66e80de5
xdrown <40439836+xdrown@users.noreply.github.com>
2018-07-07 02:54:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NDU5NDc2NjQ4MDkwMTQwNjcy.Dg7s5w.4xevr7SGmtfp0UTP6QQh3SIAT9c';
+exports.TOKEN = 'NDY0MjQ4NTMxOTc3MzcxNjQ4.DiGfkg.AJQDu2LlchWadQ94LP9TM3C3hLw';
 
-exports.PREFIX = '!>';
+exports.PREFIX = '$';
 
 exports.GOOGLE_API_KEY = 'AIzaSyC5hG9Ssu2rov-A0T_8SqQFwtAFq7Yo6Po';
